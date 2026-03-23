4519b393d46a72a52dbb3e86899ef8fe9dee49c6
titouan <titouanmok@outlook.com>
2026-03-23 11:13:58 +0100

    feat: md arrays

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,14 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:8080"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://192.168.1.172:8080",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
